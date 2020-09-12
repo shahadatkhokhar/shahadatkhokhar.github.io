a4c4d97cb804130071949cb9db36ac91fceb92d0
shahadatkhokhar <khokharshahadat@gmail.com>
2020-09-12 14:50:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const navSlide =()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click',()=>{ 
+        // toggle class name of nav-links to nav-active
+        nav.classList.toggle('nav-active')
+    
+        //animating links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation)
+        {
+            link.style.animation=``
+        }
+        else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+0.5}s`;
+         }
+         })
+        //  on opening the side pan toggle the menu burger class with toggle class
+    burger.classList.toggle('toggle')
+    })
+
+}
+
+navSlide();
